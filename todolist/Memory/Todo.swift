38f9665cf7d4c6f74b79ad605b952a751ac88338
Sonyeongha <9724syh@naver.com>
2023-08-09 23:45:28 +0900

    Step2

---
+++ todolist/Memory/Todo.swift
@@ -0,0 +1,22 @@
+//
+//  Todo.swift
+//  todolist
+//
+//  Created by 손영하  on 2023/08/09.
+//
+
+import Foundation
+import SwiftUI
+
+class Todo: Identifiable, ObservableObject {
+    let id: UUID
+    @Published var content: String
+    let insertDate: Date
+    
+    init(content: String, insertDate: Date = Date.now) {
+    id = UUID()
+        self.content = content
+        self.insertDate = insertDate
+        
+    }
+}
---
+++ todolist/Memory/Todostore.swift
@@ -0,0 +1,47 @@
+//
+//  Todostore.swift
+//  todolist
+//
+//  Created by 손영하  on 2023/08/09.
+//
+
+import Foundation
+
+class Todostore: ObservableObject {
+    @Published var list: [Todo]
+    
+    init() {
+        list = [
+            Todo(content: "Hello", insertDate: Date.now),
+            Todo(content: "Awesome", insertDate: Date.now.addingTimeInterval(3600 * -24)),
+            Todo(content: "SwiftUI", insertDate: Date.now.addingTimeInterval(3600 * -44))
+        ]
+    }
+    
+    func insert(memo: String) {
+        list.insert(Todo(content: memo), at : 0)
+    }
+    
+    func update(memo: Todo?, content: String) {
+        guard let memo = memo else { /**옵셔널을 풀어주기 위해 바인딩하기**/
+            return
+        }
+        memo.content = content
+    }
+    
+    
+    
+    /**메모와 인덱스를 받는 버전 구현하기**/
+    func delete(memo: Todo) {
+        list.removeAll {$0.id == memo.id }
+    }
+    
+    func delete(set: IndexSet) {
+        for index in set {
+            list.remove(at: index )
+        }
+    }
+    
+    
+}
+
