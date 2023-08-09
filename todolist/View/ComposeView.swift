c804fe9a1f6aab56734c7e5a9c0fbdaf1452dd4b
Sonyeongha <9724syh@naver.com>
2023-08-10 01:00:50 +0900

    Step5

---
+++ todolist/View/ComposeView.swift
@@ -0,0 +1,57 @@
+//
+//  ComposeView.swift
+//  todolist
+//
+//  Created by 손영하  on 2023/08/10.
+//
+
+import SwiftUI
+
+struct ComposeView: View {
+    @EnvironmentObject var store : TodoStore
+    
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var content: String = "" /**Stateavailable 속성이라고함**/
+    
+    
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextEditor(text: $content) /**텍스트 에디터를 사용하면 텍스트 에디터가 바인딩되고 입력한 내용이 컨텐트속성에 자동으로 저장됨**/
+                    .padding()
+            }
+            .navigationTitle("내용입력")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading)
+                {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("삭제")
+                    }
+                }
+                ToolbarItem(placement:
+                        .navigationBarTrailing)
+                {
+                    Button {
+                        store.insert(memo: content)
+                        
+                        dismiss()
+                    } label: {
+                        Text("저장")
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ComposeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ComposeView()
+            .environmentObject(TodoStore())
+    }
+}
